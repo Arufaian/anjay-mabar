1f3233575af1d81d204e8a4257feacd11dcb5203
Arufaian <alfianilyasya13@gmail.com>
2025-11-14 20:10:11 +0700

    adjust styling for reset password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,65 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+    <h2 class="card-title justify-center mb-4">{{ __('Reset Password') }}</h2>
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    <div class="w-full flex justify-center">
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+        <form method="POST" action="{{ route('password.store') }}">
+            @csrf
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <!-- Password Reset Token -->
+            <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
+            <!-- Email Address -->
+            <div class="form-control">
+                <label class="input validator">
+                    <x-lucide-mail class="h-[1em] opacity-50" />
+                    <input id="email" type="email" name="email" :value="old('email', $request->email)" required
+                        placeholder="mail@site.com" required autofocus autocomplete="username" />
+                </label>
+                <div class="validator-hint hidden">Enter valid email address</div>
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+            <!-- Password -->
+            <div class="form-control mt-4">
+                <label class="input validator">
+                    <x-lucide-lock class="h-[1em] opacity-50" />
+                    <input name="password" id="password" type="password" required placeholder="Password" minlength="8"
+                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                        title="Must be more than 8 characters, including number, lowercase letter, uppercase letter" autocomplete="new-password" />
+                </label>
+                <p class="validator-hint hidden">
+                    Must be more than 8 characters, including
+                    <br />At least one number <br />At least one lowercase letter <br />At least one uppercase letter
+                </p>
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
+            <!-- Confirm Password -->
+            <div class="form-control mt-4">
+                <label class="input validator">
+                    <x-lucide-lock class="h-[1em] opacity-50" />
+                    <input id="password_confirmation" type="password" name="password_confirmation" required placeholder="Confirm Password" minlength="8"
+                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                        title="Must be more than 8 characters, including number, lowercase letter, uppercase letter" autocomplete="new-password" />
+                </label>
+                <p class="validator-hint hidden">
+                    Must be more than 8 characters, including
+                    <br />At least one number <br />At least one lowercase letter <br />At least one uppercase letter
+                </p>
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            </div>
+
+            <div class="form-control mt-6 w-full">
+                <button type="submit" class="btn btn-primary w-full">
+                    {{ __('Reset Password') }}
+                </button>
+            </div>
+        </form>
+
+    </div>
+
 </x-guest-layout>
