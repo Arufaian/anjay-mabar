93ef17a0c4ab2974f036d6b63e7b9676b72009e8
Arufaian <alfianilyasya13@gmail.com>
2025-11-28 09:20:03 +0700

    feat: add role migration for table user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Alfian',
             'email' => 'test@example.com',
+            'password' => bcrypt('@Admin123'),
+            'role' => 'admin',
         ]);
     }
 }
