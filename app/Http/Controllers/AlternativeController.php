04f8e4aba2ddea32c656d2a87f2840bf63a1cb63
Arufaian <alfianilyasya13@gmail.com>
2025-12-11 14:56:14 +0700

    feat: implement edit alternative

--- app/Http/Controllers/AlternativeController.php
+++ app/Http/Controllers/AlternativeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreAlternativeRequest;
+use App\Http\Requests\UpdateAlternativeRequest;
 use App\Models\Alternative;
 use Illuminate\Http\Request;
 
@@ -92,15 +93,23 @@ class AlternativeController extends Controller
      */
     public function edit(Alternative $alternative)
     {
-        //
+        return view('admin.alternative.edit', [
+            'alternative' => $alternative,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Alternative $alternative)
+    public function update(UpdateAlternativeRequest $request, Alternative $alternative)
     {
-        //
+        $validated = $request->validated();
+        
+        $alternative->update($validated);
+
+        return redirect()
+            ->route('admin.alternative.index')
+            ->with('success', 'Alternative "'.$alternative->name.'" has been updated successfully.');
     }
 
     /**
