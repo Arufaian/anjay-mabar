500276c109a5b35a27798030d5ecbaecf2fe9108
Arufaian <alfianilyasya13@gmail.com>
2025-12-17 15:02:09 +0700

    chore: update database seeder for user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,12 +12,31 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Alfian',
-            'email' => 'test@example.com',
-            'password' => bcrypt('@Admin123'),
-            'role' => 'admin',
-        ]);
+        User::factory()->createMany([
+            [
+                'name' => 'Alfian',
+                'email' => 'test@example.com',
+                'password' => bcrypt('password'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@example.com',
+                'password' => bcrypt('password'),
+                'role' => 'user',
+                'email_verified_at' => now(),
+            ],
+            [
+                'name' => 'Owner',
+                'email' => 'owner@example.com',
+                'password' => bcrypt('password'),
+                'role' => 'owner',
+                'email_verified_at' => now(),
+            ],
+        ],
+        
+    );
 
     }
 }
